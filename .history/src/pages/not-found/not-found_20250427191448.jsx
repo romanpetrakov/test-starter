9b4ef8f7845f9693294b03b0a4e6c9b08a2eb25c
Roman Petrakov <namor.prs@gmail.com>
2025-05-05 15:18:30 +0300

    step3 ignore history

--- .history/src/pages/not-found/not-found_20250427191448.jsx
+++
@@ -1,9 +0,0 @@
-
-export const NotFoundPage = ({}) => {
-	return (
-		<div className={`${s.wrapper} text text_type_digits-large`}>
-			NotFound404
-		</div>
-	);
-};
-
--- .history/src/pages/not-found/not-found_20250503103052.jsx
+++
@@ -1,9 +0,0 @@
-
-export const NotFoundPage = () => {
-	return (
-		<div className="text text_type_digits-large">
-			NotFound404
-		</div>
-	);
-};
-
